7570be3f6802a3df9ab55d3886464d5877384a45
Soroush Rasti <rasti.soroush@gmail.com>
2020-10-15 11:56:20 +0330

    changing back

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 const express = require('express');
 const bycrpt=require('bcrypt-nodejs');
 const knex=require('knex');
@@ -8,22 +16,28 @@ const image = require('./controllers/image');
 
 const cors=require('cors');
 
-const db= knex({ client:'pg',connection:process.env.DATABASE_URL, ssl:true });
+const db= knex({
+    client:'pg',
+    connection:process.env.DATABASE_URL,
+    ssl:true
+});
 //const db= knex({   client:'pg',connection:{
 //    host:'127.0.0.1',database:'smart-brain'
 //}});
+
 
 const app=express();
 app.use(express.json());
 app.use(cors());
 app.get('/',(req,res)=>{
-    res.send('server back-end is working properly')
+    res.send('it is working without any problem')
 })
 
 app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bycrpt)})
 app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bycrpt)})
 app.get('/profile/:id',(req,res)=>{profile.handleProfileGet(req,res,db)})
-app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
+app.put('/image',(req,res)=>{image.handleImage(req,res)})
 app.post('/imageurl', (req,res)=>{image.handleApicall(req,res)})
 app.listen(process.env.PORT || 3000,()=>{
-console.log('server is running')})
+    console.log('server is running listening')})
+
